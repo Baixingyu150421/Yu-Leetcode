929b1307613107042609bc70c91bb428fd7812ba
BXY <SYNU_B_X_Y@163.com>
2019-05-20 16:27:38 +0800

    题目: 修改旋转数组

--- src/rotate_0189.java
+++ src/rotate_0189.java
@@ -9,8 +9,8 @@ public class rotate_0189 {
     }
 
     public static void main(String[] args) {
-        int[] nums = {-1,-100,3,99};
-        rotate(nums,2);
+        int[] nums = {1,2,3,4,5,6,7};
+        rotate2(nums,3);
         for(int a :nums){
             System.out.print(a + " ");
         }
@@ -27,13 +27,21 @@ public class rotate_0189 {
      * @param nums
      * @param k
      */
-    public  void rotate2(int[] nums, int k) {
+    public static   void rotate2(int[] nums, int k) {
         int time = nums.length - 1;
         for(int i = 0 ; i < k ; i++){
             int temp = nums[time];
-            while (time > 0){
-                nums[time] = nums[time --];
-            }
+            nums[time] = nums[time--];
+            nums[0] = temp;
+        }
+    }
+    //复制数组
+    public static void rotate3(int [] nums , int k){
+        int n = nums.length;
+        k %= n;
+        for(int i = 0 ; i < k ; i++){
+            int temp = nums[nums.length - 1];
+            System.arraycopy(nums,0,nums,1,n-1);
             nums[0] = temp;
         }
     }
